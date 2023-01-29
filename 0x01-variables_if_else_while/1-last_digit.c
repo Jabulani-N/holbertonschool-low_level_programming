7d7c4e55362d27ca49909d9963d0c20af646a396
Jabulani-N <jabu.94@gmail.com>
2023-01-29 13:16:30 -0800

    remainder_example_in_c_and_also_if_logic

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,27 +10,34 @@
  * this basically spams the relevant if statements.
  *Current project task 1:
  * before spamming those relevant if statements, it'll first announce the last digit of n.
- *we'll save that last digit in a nother variable, and then edit the relevant if statement spam from the previous question to apply to this new variable and criteria.
+ *we'll save that last digit in a nother variable,
+ * and then edit the relevant if statement spam from the previous question
+ *   to apply to this new variable and criteria.
+ *The moludlo (%) is gonna be our friend here.
+ * x % y gives the remainder of x/y.
+ *  when y=10, that essentially means the last digit.
  */
 int main(void)
 {
 
 int n;
+int lastDigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-printf("%i ", n);
-if (n == 0)
+lastDigit = (n % 10);
+printf("Last digit of %i is %i ", n, lastDigit);
+if (lastDigit == 0)
 {
-printf("is zero\n");
+printf("and is 0\n");
 return (0);
 }
-if (n > 0)
+if (n > 5)
 {
-printf("is positive\n");
+printf("and is greater than 5\n");
 return (0);
 }
-printf("is negative\n");
+printf("and is less than 6 and not 0\n");
 return (0);
 }
