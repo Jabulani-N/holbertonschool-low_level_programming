7c7d44d5571294351d9aefadc4eea70b8947e090
Jabulani-N <jabu.94@gmail.com>
2023-02-03 14:37:43 -0800

    see_previoust_tasks_islower

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/*including main.h makes it scan main.h, & include all functions within*/
+/*within main.h is a list of every function my functions will call*/
+/* more headers goes there */
+/**
+ * _isupper - just see if the number is lower than the minimum uppercase value.
+ *if so, see if it's lower than the maximum uppercase ascii.
+ *otherwise, return the 'no it's not' output.
+ *This script is copied from the previous Sprint assignmtnts's _islower
+ * @c: the character we'll be judging the status of
+ * Return: depends
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+instead of printing text normally, you need to send the characters to the _putchar function and have it do the printing instead.
+
+This README is copied from teh 0x02 README.md
+
+
