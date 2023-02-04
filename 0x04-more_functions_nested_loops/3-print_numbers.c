c605d67df38d2887f395f5be05779cccc7c9ed30
Jabulani-N <jabu.94@gmail.com>
2023-02-03 16:47:47 -0800

    for_loop,_finding_a_char_without_needing_its_ascii_number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/*including main.h makes it scan main.h, & include all functions within*/
+/*within main.h is a list of every function my functions will call*/
+/* more headers goes there */
+/**
+ * print_numbers - prints numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i='0'; i<='9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return;
+}
