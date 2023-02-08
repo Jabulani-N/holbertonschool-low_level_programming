3f2bfb51d21b44f7c9c78a392fa0613efa41f6eb
Jabulani-N <jabu.94@gmail.com>
2023-02-08 15:41:03 -0800

    see_task_0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * *_strncat - content of a destination
+ * replaces the terminating '\0' from *dest and repalces it with *src
+ * cant take more than n bytes from src.
+ * note *src will bring it's own terminating '\0'
+ * @dest: this guy will get modified
+ * @src: this guy will get read
+ * Return: the changed variable.
+ * the return format will cause it to become a pointer
+ * This is copied from Task 0
+ **/
+
+/*get dest's size so we can append to it's final position*/
+/*iterate through src to slap it's contetns, starting at dest's final pose*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destSize;
+	int srcPosition;
+
+	destSize = 0;
+	srcPosition = 0;
+
+	while (dest[destSize] != '\0' && destSize =< (n / 4))
+	/* a char is 8 bytes, so hte size/8 = number of chars allowed*/
+	{
+		destSize++;
+	}
+	/*we now know destSize, and can append starting at the final position*/
+	/*we'll use srcPosition to check and make sure we stop at the end or src*/
+	while (src[srcPosition] != '\0')
+	{
+		dest[destSize + srcPosition] = src[srcPosition];
+		srcPosition++;
+	}
+return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,7 @@
 this readme will have some notes
 
 
+in question 1-strncat, we'll copy the previous question except:
+
+we're gonna change the while condition on the srcSize finding loop to include AND the size is =< than the ammount alloted by n
+- n will be an integer, but the previous task's quiz tells us the byte-size of chars. we can do in-if-statment math to find the position cap
