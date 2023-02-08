aac068e7f9cf2ab756ad81c6b44ce0d298a98f58
Jabulani-N <jabu.94@gmail.com>
2023-02-08 15:23:48 -0800

    returning_poionter._pointer_inputs

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,6 +5,8 @@
  * note *src will bring it's own terminating '\0'
  * @dest: this guy will get modified
  * @src: this guy will get read
+ * Return: the changed variable.
+ * the return format will cause it to become a pointer
  **/
 
 /*get dest's size so we can append to it's final position*/
@@ -20,7 +22,7 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[destSize] != '\0')
 	{
-		destSize++;	
+		destSize++;
 	}
 	/*we now know destSize, and can append starting at the final position*/
 	/*we'll use srcPosition to check and make sure we stop at the end or src*/
