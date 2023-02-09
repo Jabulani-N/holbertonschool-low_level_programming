a59cf9a54fdac4376bffbc06d0637523dcc0c26c
Jabulani-N <jabu.94@gmail.com>
2023-02-09 15:53:43 -0800

    string_and_array_operation

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -18,7 +18,9 @@ void reverse_array(int *a, int n)
 	/*UQHolder is int because the array holds int*/
 	int position;
 
-	for (position = 0; position <= (n / 2); position++)
+	for (position = 0; position <= (n / 2 + 1); position++)
+		/*+1 to counteract the position -1 that exists below*/
+		/*counteract as in 'make sure everything gets counted'*/
 	{
 		UQHolder = a[position];
 		a[position] = a[n - position - 1];
