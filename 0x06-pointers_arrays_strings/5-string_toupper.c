aa3944c9a7fe21a19ef9a08a79c9fa546ea80182
Jabulani-N <jabu.94@gmail.com>
2023-02-10 13:58:56 -0800

    iterate_via_variable

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * *_string_toupper - if target is lowercase, make it uppercase
+ * *string_toupper - if target is lowercase, make it uppercase
  * for loop through the string array, and if entry is within lowercase
  * then add enough to make it uppercase (same for every letter)
- * @s: the string in question. char array
+ * @input: the string in question. char array
  * Return: the changed char array
  **/
 
@@ -11,16 +11,13 @@
 /**/
 char *string_toupper(char *input)
 {
-	/*char output[];*/
-	/*it doesn't liek unknown rray size, so let's jsut directly change the source and re-output it*/
 	int size;
 	int pos;
 
 	size = 0;
 	for (pos = 0; input[pos] != '\0'; pos++)
 	{
-		size ++;
-		
+		size++;
 	}
 
 	for (pos = 0; pos <= size; pos++)
