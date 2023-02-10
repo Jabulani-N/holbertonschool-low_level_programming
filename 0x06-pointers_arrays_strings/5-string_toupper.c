0d7636e5c3cd6542d5ca61bf724fffeb9b2b7de4
Jabulani-N <jabu.94@gmail.com>
2023-02-10 12:36:45 -0800

    checking_intended_function_prototype

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * *_string_toupper - if target is lowercase, make it uppercase
+ * for loop through the string array, and if entry is within lowercase
+ * then add enough to make it uppercase (same for every letter)
+ * @input: the string in question. char array
+ * Return: the changed char array
+ **/
+
+/**/
+/**/
+char *string_toupper(char *input)
+{
+while *input
+}
