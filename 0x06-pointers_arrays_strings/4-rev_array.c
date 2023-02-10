2f1b74f65d291b794ae62d5a3533a07f0b1df5f2
Jabulani-N <jabu.94@gmail.com>
2023-02-10 12:04:37 -0800

    if_statement_to_handle_n=2

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,19 +14,28 @@
 
 void reverse_array(int *a, int n)
 {
-	int Holder;
-	/*Holder is int because the array holds int*/
+	int ZentaiHolder; /*zentai: "all target" holder used everywhere*/
+	/*ZentaiHolder is int because the array holds int*/
 	int position;
+
+	if (n == 2)
+	{
+		ZentaiHolder = a[0];
+		a[0] = a[1];
+		a[1] = ZentaiHolder;
+	}
 
 	for (position = 0; position <= (n / 2); position++)
 	{
-		Holder = a[position];
+		ZentaiHolder = a[position];
 		a[position] = a[n - position - 1];
 		/* -1 so it doesn't interact with the terminating null charr*/
-		a[n - position - 1] = Holder;
+		a[n - position - 1] = ZentaiHolder;
 		/*-1 so the place we pulled from is the place that gets replaced*/
 	}
 	/*note current code BREAKS IF SIZE = 2*/
 	/*find what to do if position = 1. may becme if-statement*/
+	/*Done in if-statement befor ethis for loop.*/
+	/*the for loop won't ruin the two becuase it skips 2s for some reaosn*/
 return;
 }
