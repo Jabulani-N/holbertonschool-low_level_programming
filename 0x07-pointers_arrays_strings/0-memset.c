bf2db15370f67f052cd69fba21bf65f896b6dee4
Jabulani-N <jabu.94@gmail.com>
2023-02-12 17:55:17 -0800

    replace_first_n_slots_of_inputted_array_char

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+/**
+ * _memset - replaces memory locale s's content with char b for n positions
+ * @s: memory location. star makes us recieve it's contents
+ * @b: replaces content of memory location s
+ * @n: number of bytes of s to replace with b
+ * Return: content of s (pointer to s)
+ *
+ * because s is a char, it'll have to follow char rules
+ * 	that means we can treat it like any other array (hope)
+ **/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while(n--)/*n does not equal zero is n being true*/
+	/*it'll read the -- after it's already used the current value*/
+	{
+		s[n - 1] = b;
+		/*replaces all content until slot 0*/
+		/*-1 allows it to hit 0 despite n never hitting 0*/
+		/*	also protects the terminating null at position initial n*/
+	}
+	return (*s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+this readme will have some notes
+
+
+according to man memset, we're changing the content of memory location s. we can do that via *s, which manipulates the content itelf
+
