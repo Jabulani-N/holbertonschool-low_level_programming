16e034f26a17c79c4556000b9f42d5e29785291a
Jabulani-N <jabu.94@gmail.com>
2023-02-13 17:01:13 -0800

    Betty-style_and_altered_while

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,22 +6,24 @@
  * Return: content of s (pointer to s)
  *
  * because s is a char, it'll have to follow char rules
- * 	that means we can treat it like any other array (hope)
+ *	that means we can treat it like any other array (hope)
  **/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n--)/*n does not equal zero is n being true*/
+	while (n > 0)/*n does not equal zero is n being true*/
 	/*it'll read the -- after it's already used the current value*/
 	{
 		s[n - 1] = b;
+		n--;
 		/*replaces all content until slot 0*/
 		/*-1 allows it to hit 0 despite n never hitting 0*/
 		/*	also protects the terminating null at position initial n*/
 	}
 	return (s);
 }
-/*known bugs
+/*
+ * known bugs
  *
  *segmentation fault when run.
  *	consider altering iteration that runs n times
