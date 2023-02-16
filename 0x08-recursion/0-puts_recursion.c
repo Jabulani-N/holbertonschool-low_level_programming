fe002867bb53026ae7c081bfb5442c23b05381a0
Jabulani-N <jabu.94@gmail.com>
2023-02-15 17:09:42 -0800

    _puts_then_recurse

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+/**
+ * _puts_recucrsion(char *s) - scans char array s
+ *	_putchar the first part of the array
+ *		then shove the remaining letters into recursion
+ *		we can do this via sending it s++, or s+1. or s >= 1
+ *		this is because sending  an array this way sends only positions 1 and up
+ *		remember the first slot in an array is position 0
+ *
+ * @s: char array in question
+ * Return - Always Void
+ **/
+
+void _puts_recursion(char *s)
+{
+	if (s)/*if it works for while, just maybe we can if it*/
+	{
+		_putchar(s);
+		_puts_recursion(s >= 1)
+	}
+	return;
+}
