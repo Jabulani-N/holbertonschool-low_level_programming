5e1809a214b4b13ac817594f1c1bcba953acc0f4
Jabulani-N <jabu.94@gmail.com>
2023-02-16 19:44:20 -0800

    first_try_exponent_via_task3_edit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,33 +1,30 @@
 /**
- * factorial - ultimately returns factorial of input int
- *	calls self and givs it input-1
- *a factorial is a number times all numbers less than it until 0
- * for example, 5! = 5x4x3x2x1
- * @n: find my factorial, please♥
- * Return: input x return of self(input-1) as many times as recursed,
- *	but only if input is 1 or higher
- *	This simulates factorial calculation brute force
- *Returns 1 if input was 0 (0 factorial is 1)
+ * _pow_recursion - multiplies x by itself y times
+ *	calls self and givs it x, y-1
+ *power is number times itself
+ * for example, 5^5 = 5x5x5x5x5
+ * @x: number to be multiplied♥
+ * @y; number of times to multiply x
+ * Return: x times self,y-1
+ *	but only if y is 0 or higher
  *Returns -1 ifi nput was negative
  *
  *
- *copied form Task 2,
- *changed it from a sum to a product
- *cahnged it to make 0 input a special case
- *changed it to make <0 a special case
- *changed it to work on integers instead of char arrays
+ *copied form Task 3,
+ *changed it to appropriate variables.
+ *minor changes to represent exponents
  **/
 
 #include "main.h"
 
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n > 0)/*there is still some left to work with*/
+	if (y > 0)/*there is still some left to work with*/
 	{
-		return (n * factorial(n - 1));/*recurse*/
+		return (x * _pow_recursion(x, y - 1));/*recurse*/
 	}
-	else if (n == 0) /*las consideration*/
-		return (1);/*0! = 1, so this is correct even if we started with n=0*/
+	else if (y == 0) /*last consideration*/
+		return (1);/*x^0 = 1, so this is correct even if we started with y=0*/
 	else/*neither 0 nor greater than 0 musst be negative*/
 		return (-1);
 }
