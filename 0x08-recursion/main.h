df9edc28ef93df187c32b19b32cce48c59bd06a3
Jabulani-N <jabu.94@gmail.com>
2023-02-16 23:31:04 -0800

    clean_my_comments

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_recursion_sub (int base, int testRoot);
 int is_prime_number(int n);
-int  _prime(int x, int y, int numInQuestion);
+int  _prime(int x, int numInQuestion);
 
