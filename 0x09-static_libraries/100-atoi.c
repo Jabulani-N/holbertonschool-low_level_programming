da9b814bf050fe228f3787dd99394d8e9560d468
Jabulani-N <jabu.94@gmail.com>
2023-02-17 18:38:13 -0800

    more_cleaning

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+/**
+ * _atoi - who knows what this was intended to do
+ *@s: who knows what this was intnded to do
+ *Return: always 0
+ */
+
+int _atoi(char *s)
+{
+	return(0);/*placeholder empty*/
+}
