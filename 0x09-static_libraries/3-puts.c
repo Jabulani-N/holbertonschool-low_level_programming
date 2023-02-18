e282bac0c49716b252bd3141a3daf3e6ad71cb69
Jabulani-N <jabu.94@gmail.com>
2023-02-17 19:45:10 -0800

    copied_anothers_code

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -7,23 +7,12 @@
  * Return: Void
  * basically copied form the previous task
  **/
+
 void _puts(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-		/*when we referenc the array itself, we already have access to it*/
-		/*so we don't need th asterik anymore*/
-	{
-	_putchar(str[i]);
-/*consider going back to where you wrote a _putchar*/
-/*to remind yourself how to print to standard output*/
-	i++;
-	}
-	_putchar('\n');
-		return;
-	/*like the strlen manual entry says,*/
-	       /*we're gonna go until we hit the "terminating null byte '/0'"*/
+  for (; *str != '\0'; str++)
+    {
+      _putchar(*str);
+    }
+  _putchar('\n');
 }
-
