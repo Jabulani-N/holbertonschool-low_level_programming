1cb1b5490d84fa0bc4786ab697513addc1b769e0
Jabulani-N <jabu.94@gmail.com>
2023-02-18 19:25:03 -0800

    index_within_args.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - create heap
@@ -17,20 +19,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int heap;/*the sum*/
+	int heap = 0, i, j;/*the sum, a counter, a counter*/
+	j = argc;
 
-	while (*argv)
+	while (j-- > 1)/*0 breaks it*/
+/*this whole thing is to check the all arguments are numbers*/
+		/*argument level. whole string*/
 	{
-		if (isnumber(*argv))
+		/*about to check it's digits*/
+		for (i =0; argv[j][i]/*while this is valid*/;i++)
+		/*single character level of an argument*/
 		{
-			heap = (eap + atoi(argv);
-			argv++;
+			if (!(isdigit(argv[j][i])))
+			{
+				printf("Error\n");
+				return(1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		heap = (heap + atoi(argv[j]));
 	}
 
 	printf("%d\n", heap);
