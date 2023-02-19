769184d1f19f506a6dc433b6528ea3c10b0a0bdf
Jabulani-N <jabu.94@gmail.com>
2023-02-18 16:44:54 -0800

    how2atoi-turn_string_to_int

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check ifi 2 input args (argsc=3)
+ * 	if so, multiply
+ * 	if not, say 'error'
+ *
+ * argv[0] is function name. later positions have arguments made
+ *
+ *@argv: program name followed by input arguments.
+ *	vector.
+ *@argc: number of input arguments
+ *
+ *	Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		argv++;
+	}
+	else
+		printf("error\n");
+	return (0);
+}
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
 void myNAMEis(int argc, char *argv[]);
+int _putchar(char c);
 #endif
 
