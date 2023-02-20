0fc930ad2c5e7914eca597b7e3713d3a3ac8429a
Jabulani-N <jabu.94@gmail.com>
2023-02-19 20:24:24 -0800

    betty-style_compliance

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,14 +1,20 @@
-/*it looks like this first task is essentially to make a 1Dimension array of the given size, and fill each slot with the given char (one char)
- * We have to malloc the space we're gonna use. when you memory allocate (malloc), it'll return a pointer to where that memory is.
- * 
+/*
+ * it looks like this first task is essentially to make a 1Dimension array
+ * of the given size, and fill each slot with the given char (one char)
+ *
+ * We have to malloc the space we're gonna use.
+ * when you memory allocate (malloc),
+ *	it'll return a pointer to where that memory is.
+ *
  */
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
  * create_array - preallocates memory, and then makes an array
- * 	if the size requested is 0: allocate null, return null
- * 	otherwise, take the pointer to the preallocation, and shove the arary into it
+ *	if the size requested is 0: allocate null, return null
+ *	otherwise, take the pointer to the preallocation,
+ *	and shove the arary into it
  *
  * @size: the requested array size
  * @c: character to shove into each slot of the array
@@ -23,9 +29,10 @@ char *create_array(unsigned int size, char c)
 	/*pointer so base can be an address*/
 	preallocated = malloc(size * sizeof(char));
 	/*preallocated has become a pointer to the allocated memory*/
-	/*it seems to have also kept the char trait,
-	 * 	so the mem is allocated to char preallocaetd
-	 * 		I think that this has effectively turned it into char array
+	/*
+	 * it seems to have also kept the char trait,
+	 *	so the mem is allocated to char preallocaetd
+	 *		I think that this has effectively turned it into char array
 	 */
 
 	if (preallocated == NULL)
@@ -34,7 +41,7 @@ char *create_array(unsigned int size, char c)
 	 * returns null if pointer was to null
 	 * I guess this is a redundancy?
 	 */
-	
+
 
 	if (size == 0)
 		return (NULL);
@@ -51,5 +58,5 @@ char *create_array(unsigned int size, char c)
 		/*shoves requested char into place*/
 		index++;
 	}
-	return(preallocated);
+	return (preallocated);
 }
