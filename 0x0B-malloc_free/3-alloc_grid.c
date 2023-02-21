0c34d072863305957cd5fc5216d8934f3db97668
Jabulani-N <jabu.94@gmail.com>
2023-02-20 20:16:28 -0800

    betty-style_compliance

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,27 +7,36 @@
  * @width: An input integer at number of columns
  * @height: An input integer at number of rows
  * Return: pointer to a 2D array, NULL on failure
- * directly copied from another's repo
+ * directly copied from another's repo,
+ * but with
+ *	added "free" commands
+ *	betty-style compliance
  */
 int **alloc_grid(int width, int height)
 {
-  int **array, i = 0, j = 0;
+	int **array, i = 0, j = 0;
 
-  if (width <= 0 || height <= 0)
-    return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-  array = (int **)malloc(sizeof(int *) * height);
-  if (array == NULL)
-    return (NULL);
-  for (; i < height; i++)
-    {
-      array[i] = (int *)malloc(sizeof(int) * width);
-      if (array[i] == NULL)
-	return (NULL);
-    }
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	{
+	for (; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
+			free(array);
+			return (NULL);
+		}
+	}
 
-  for (i = 0; i < height; i++)
-    for (; j < width; j++)
-      array[i][j] = 0;
-  return (array);
+	for (i = 0; i < height; i++)
+		for (; j < width; j++)
+			array[i][j] = 0;
+	return (array);
 }
