5f7540ffa81a5755cd136a47e8d72de827f5c269
Jabulani-N <jabu.94@gmail.com>
2023-02-20 19:39:47 -0800

    modified_from_task1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,7 +33,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int s1Size = 0;
+	unsigned int subIndex = 0;
 	unsigned int magicalIndex = 0;/*for safety's sake*/
 	/*will be multi-use, so BE SURE TO RESET*/
 	char *preallocated;
@@ -44,25 +44,30 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 		/*null counts as empty string*/
-	while (str[magicalIndex] != '\0')
+	while (s1[magicalIndex])
+		magicalIndex++;/*we now know size of s1*/
+	while (s2[subIndex])
+	{
 		magicalIndex++;
+		subIndex++;
+	}
 	preallocated = malloc((magicalIndex + 1) * sizeof(char));
 	/*+1 makes it include the slot for terminating NULL*/
-	/*preallocated has become a pointer to the allocated memory*/
-	/*it seems to have also kept the char trait.*/
 	if (preallocated == NULL)
 		return (NULL);/*catch malloc errors*/
-/*	if (size == 0)*//*don't care if size is 0*/
-/*		return (NULL);*/
-	/*return null if size 0 (useless array)*/
-/*so now that we know the target and array will be valid...*/
 	/*reset index*/
 	magicalIndex = 0;
-	while (str[magicalIndex])
+	while (s1[magicalIndex])
 	{
-		preallocated[magicalIndex] = str[magicalIndex];
+		preallocated[magicalIndex] = s1[magicalIndex];
 		/*shoves requested char into place*/
 		magicalIndex++;
-	}
+	}/*we're at the end of s1's content, excluding NULL*/
+	while (s2[subIndex])
+	{
+		preallocated[magicalIndex] = s2[subIndex];
+		magicalIndex++;
+		subIndex++
+	}/*it'll prolly end on NULL by default*/
 	return (preallocated);
 }
