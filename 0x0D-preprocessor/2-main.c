1118d27aa27d30c64446731eb2dfb5e03a223f2f
Jabulani-N <jabu.94@gmail.com>
2023-02-22 09:57:25 -0800

    try1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,25 @@
+/**
+ *main - returns the name of the file it will be compiled from
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("__FILE__\n");/*__FILE__ is a predefined macro*/
+	/*it expands into the name of the current inputfile*/
+	/*name as in "whole entire address"*/
+	/*
+	 * since the complier/preprocessors can onlypossibly be reading
+	 * these lines in this file while it is reading this file,
+	 * that essnetially means
+	 * "name/address of this file we humans are reading right now"
+/*
+ * further reading at
+ * https://gcc.gnu.org/onlinedocs/gcc-5.1.0/cpp/Standard-Predefined-Macros.html#Standard-Predefined-Macros
+ */
+	return(EXIT_SUCCESS);
+}
+
