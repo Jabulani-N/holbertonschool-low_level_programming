0eade53299b0d47ca6559fabeeb94fae6d159fdf
Jabulani-N <jabu.94@gmail.com>
2023-03-02 16:53:23 -0800

    access_structure_elements

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,5 @@
 /**
- *
- *
- *init_dog - shoves values into a dog structure
+ * init_dog - shoves values into a dog structure
  *
  *struct dog - struct with name, owner, age,
  *@d: dog in question. an address.
@@ -19,10 +17,22 @@
 #include <string.h>
 
 
+/*
+ *so the struct dog mydog has already been done in 1-main.c
+ *we get passed a pointer of it's adress
+ *	This allows us to access mydog's elements from 1-main.c
+ *		by messing with *d.element = elementContent
+ *
+ * I do not want to declare d, because d aleady exists.
+ * 	I only want to edit d's elements
+ *
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog *d = {.name = *name, .age = age, .owner = *owner};
-	return;
+	/*example of two different ways to access elements*/
+	(*d).name = name;
+	d->owner = owner;
+	d->age = age;
 }
 
 
