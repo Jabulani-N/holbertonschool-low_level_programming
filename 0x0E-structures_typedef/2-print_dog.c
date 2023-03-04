fbc10ca1336c8c1d9409d2fa4cbfbb09270c60e3
Jabulani-N <jabu.94@gmail.com>
2023-03-03 17:00:23 -0800

    test_without_catching_null_age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,7 @@
 #include "dog.h"
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 
 /*
@@ -34,9 +35,9 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
         if(d->owner == NULL)
 	                d->owner = "(nil)";
-	if (d->age == NULL)
-		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
-	else
+/*	if (d->age == NULL)*/
+/*		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);*/
+/*	else*/
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 
