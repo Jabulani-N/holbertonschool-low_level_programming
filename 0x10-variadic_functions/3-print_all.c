e37e7fbedc0d0ad727cedf847600d9ed0a35c93a
Jabulani-N <jabu.94@gmail.com>
2023-03-06 22:06:01 -0800

    readability_update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,45 +10,45 @@
  */
 void print_all(const char * const format, ...)
 {
-va_list ap;
-char *temp;
-int i = 0;
+	va_list ap;
+	char *temp;
+	int i = 0;
 
-va_start(ap, format);
-while (format == NULL)
-{
-printf("\n");
-return;
-}
-while (format[i])
-{
-switch (format[i])
-{
-	case 'c':
-		printf("%c", (char) va_arg(ap, int));
-		break;
-	case 'i':
-	printf("%d", va_arg(ap, int));
-	break;
-	case 'f':
-	printf("%f", (float) va_arg(ap, double));
-	break;
-	case 's':
-	temp = va_arg(ap, char*);
-	if (temp != NULL)
+	va_start(ap, format);
+	while (format == NULL)
 	{
-	printf("%s", temp);
-	break;
+		printf("\n");
+		return;
 	}
-	printf("(nil)");
-	break;
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", (char) va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", (float) va_arg(ap, double));
+				break;
+			case 's':
+				temp = va_arg(ap, char*);
+				if (temp != NULL)
+				{
+					printf("%s", temp);
+					break;
+				}
+				printf("(nil)");
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+			format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
+		i++;
 	}
-if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
-format[i] == 's') && format[(i + 1)] != '\0')
-printf(", ");
-i++;
-}
-va_end(ap);
-printf("\n");
+	va_end(ap);
+	printf("\n");
 }
 
