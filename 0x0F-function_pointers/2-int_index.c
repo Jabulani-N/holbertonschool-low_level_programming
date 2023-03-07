68b937a8440183ea4cb0a0c2e6a1dce7da9c69fc
Jabulani-N <jabu.94@gmail.com>
2023-03-06 20:40:53 -0800

    based_on_anothers_code

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,54 @@
+
+
+
+/**
+ * int_index - puts recievevd args into recieved function
+ *
+ * @array: received argument
+ * @size: recieved argument. size of array
+ * @action: received function
+ * Return: absolutely nothing
+ *	do NOT return "NULL". it will not work.
+ *
+ * Description: basically, we jsut recieve a funciton
+ * create a pointer to said funciton (it'll look liek a variable)
+ *	interact like a function
+ * you can see the main file to see the format the function wants
+ *
+ * it wants to receive each element individually
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	void (*fun_ptr)(int) = *cmp;
+	int index;
+
+	if (cmp == NULL || size <= 0 || array == NULL)
+		return (-1);
+	for (index = 0; index < size; index++)
+		if (cmp(array[index]))
+			return(index);
+	return (-1);
+
+}
+
+/*
+ * basically, just take recieved function
+ *	make sure it is valid
+ *	make sure received argument is also valid
+ * run received function with received argument
+ * that's all you need
+ *
+ *	KNOWN BUGS
+ *
+ *		FIXED
+ * There is a case of input that creates an incorrect output.
+ *	usually with arrays, this is a matter of neglecting the final position
+ *	that holds the trminating null
+ *	- it didn't consider the case of array == NULL
+ */
+
