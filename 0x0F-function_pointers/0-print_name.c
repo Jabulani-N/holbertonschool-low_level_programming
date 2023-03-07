6eed06fb53bcab214f7be9545fb9074a76f9c3de
Jabulani-N <jabu.94@gmail.com>
2023-03-06 17:32:59 -0800

    betty_compliance

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,31 +2,34 @@
 
 
 /**
- * basically we jsut recieve a funciton and some other garbage
- * creat ea pointer to said function (it'll look like a variable)
- * 	and interact like a function-like macro
- *
- *
  * print_name - purts recievevd arg1 into recieved function
  *
  * @name: received argument
  * @f: received function
  * Return: void
  *
+ * Description: basically, we jsut recieve a funciton
+ * create a pointer to said funciton (it'll look liek a variable)
+ *	interact liek a function-like macro
  */
-/*
- * basically, just take recieved function f
- * 	make sure it is valid
- * run f with the received argument name
- * 	make sure it is also valid
- * that's all you need.
- */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
 void print_name(char *name, void (*f)(char *name2))
 {
-	
 	if (f == NULL || name == NULL)
 		return (NULL);
 
 	f(name);
 }
+
+/*
+ * basically, just take recieved function f
+ *	make sure it is valid
+ * run f with the received argument name
+ *	make sure it is also valid
+ * that's all you need.
+ */
 
