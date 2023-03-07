aeb9939bc473141d14a47c53887e78a82d219cc4
Jabulani-N <jabu.94@gmail.com>
2023-03-06 19:01:47 -0800

    first_glance

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,45 @@
+
+
+
+/**
+ * print_name - purts recievevd arg1 into recieved function
+ *
+ * @array: received argument
+ * @size: recieved argument. size of array
+ * @action: received function
+ * Return: absolutely nothing
+ *	do NOT return "NULL". it will not work.
+ *
+ * Description: basically, we jsut recieve a funciton
+ * create a pointer to said funciton (it'll look liek a variable)
+ *	interact like a function
+ * you can see the main file to see the format the function wants
+ *
+ * it wants to receive each element individually
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	void (*fun_ptr)(int *) = *action;
+	int index;
+
+	if (action == NULL || size == NULL)
+		return;
+	for (index = 0; index < size; index ++)
+		fun_ptr(array[index]);
+}
+
+/*
+ * basically, just take recieved function
+ *	make sure it is valid
+ * 	make sure received argument is also valid
+ * run received function with received argument
+ * that's all you need
+ *
+ *
+ */
+
