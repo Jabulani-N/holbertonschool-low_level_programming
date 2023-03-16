bf0c2dd07c5100ea2233ea82d9f418f2a4c17d50
Jabulani-N <jabu.94@gmail.com>
2023-03-15 20:25:29 -0700

    iterate_through_linked_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+/**
+ * this funcion will be reading a list already made.
+ *
+ * size_t print_list - receives an address of a list
+ * @h - the list address in question
+ *	this makes *h the contents of that list structure
+ *		as a structure, you'll need to request the exact dataslot you want to interact with
+ *we'll need to access the next item in the chain, and so on, until one equlas NULL
+ *Return: the number of linked lists
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	list_t *UQHolder = malloc(sizeof(list_t));
+	size_t UQNumbers = 0;
+
+	if (UQHolder == NULL)
+		return (-1);
+	if (h == NULL)
+	{
+		free(UQHolder);
+		printf("[0] (nil)");
+		return(0);
+	}
+	UQHolder = h;
+	while (UQHolder != NULL)
+	{
+		printf("[%i] %s\n", UQHolder->len, UQHolder->str);
+		UQNumbers++;
+		UQHolder = UQHolder->next;
+	}
+return (UQNumbers);
+
+}
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,10 @@
+# README
+
+
+This is our Singly Linked Lists repository.
+
+Linked Lists are structures (nodes) that store both usable data, and the address of the next node in the list.
+
+- The final node will be equal to NULL
+
+
