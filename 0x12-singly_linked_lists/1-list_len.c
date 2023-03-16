00a48e6fd8eb6fecb1b9d4c453dbe74e560129d2
Jabulani-N <jabu.94@gmail.com>
2023-03-16 14:45:21 -0700

    is_Task1_just_reduced_task0?

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,43 @@
+/**
+ * list_len - receives an address of a list
+ * This function will be reading a list already made.
+ * @h: the list address in question
+ *	this makes *h the contents of that list structure
+ *		as a structure,
+ *		you'll need to request the exact dataslot you want to interact with
+ *we'll need to access the next item in the chain,
+ *	and so on, until one equlas NULL
+ *Return: the number of linked lists
+ */
+
+#include "lists.h"
+#include <string.h>
+
+size_t list_len(const list_t *h)
+{
+	list_t *UQHolder = malloc(sizeof(list_t));
+	size_t UQNumbers = 0;
+
+	if (UQHolder == NULL)
+		return (-1);
+	if (h == NULL)
+	{
+		free(UQHolder);
+		return (0);
+	}
+	*UQHolder = *h;
+	while (UQHolder != NULL)
+	{
+/*		if (UQHolder->str == NULL)*/
+/*			printf("[0] (nil)\n");*/
+/*		else*/
+/*			printf("[%i] %s\n", UQHolder->len, UQHolder->str);*/
+		UQNumbers++;
+		UQHolder = UQHolder->next;
+	}
+
+	free(UQHolder);
+	return (UQNumbers);
+
+}
+
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -7,4 +7,16 @@ Linked Lists are structures (nodes) that store both usable data, and the address
 
 - The final node will be equal to NULL
 
+# Task 1
+
+Task 0 example main file was using printf("%lu",n); where n was a malloced thing itself to denote the numebr of elements.
+
+- printf man 3 says %l makes the following unsigned/int a long format.
+
+- u makes the following int unsigned
+
+That said, that n directly equals the return of my previous Task 0, and the example calls those "elements," so maybe taht jsut means the numebr of nodes
+
+- I can certianly try that
+
 
