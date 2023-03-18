834912cb1c8b5d78678e93cc83aee0e90229b0a5
Jabulani-N <jabu.94@gmail.com>
2023-03-18 02:48:31 -0700

    checks_if_free_worked

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+/**
+ * delete_nodeint_at_index - inserts a node at index
+ * @head: *head is location of head
+ *	be sure to check for "head" nad not "*head" being NULL
+ * @index: target position. 0 is head.
+ * Return: 1
+ *	on failure: -1
+ */
+
+#include "lists.h"
+#include "6-pop_listint.c"
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *frozenTraveler, *holder;
+
+	if (head == NULL)
+		return (-1);
+	if (idx == 0)/*head deletion.*/
+	{
+		if (pop_listint(head) == 0)
+			return (-1);
+		return (1);
+		/*why do work twice?*/
+	}
+	frozenTraveler = *head;
+	while (idx > 1)
+	{
+		if (frozenTraveler->next == NULL)
+			return (NULL);/*tried to delete OOB*/
+		frozenTraveler = frozenTraveler->next;
+		idx--;
+	} /*we are now one node before where we want to delete one*/
+	/*Whirlygig Shuffle*/
+	holder = frozenTraveler->next;/*preserve original next*/
+	frozenTraveler->next = (frozenTraveler->next)->next;/*take original next's place in line*/
+	free(holder);/*delete original next*/
+	if (holder == NULL)/*ensure free worked*/
+		return (1);
+	else
+		return (-1);
+
+}
