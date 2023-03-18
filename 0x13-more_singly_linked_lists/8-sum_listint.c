3a93ab4000c4efa4170db5170e1b3c631797d29a
Jabulani-N <jabu.94@gmail.com>
2023-03-18 01:15:16 -0700

    basically_task0

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+/**
+ * sum_listint - recieves head of linked list
+ *	sums each node's n (it's an int)
+ * @head: address of head of linked list
+ * Return: sum of nodes in linked list's ns
+ *
+ * Based on Task0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+int sum_listint(listint_t *head)
+{
+	listint_t *holder;
+	int total = 0;
+
+	holder = h;
+	while (holder)/*so we don't break head for other functions*/
+	{
+		total += holder->n;
+		holder = holder->next;
+	}
+	return (total);
+}
