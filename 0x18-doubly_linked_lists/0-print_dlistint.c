b16a7ec93d87685c97ed52acf49f1de822c01099
Jabulani-N <jabu.94@gmail.com>
2023-03-23 17:56:28 -0700

    included_lists.h

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -7,6 +7,8 @@
  *	on failure: EXIT_FAILURE
  */
 
+#include "lists.h"
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *hold;
--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 /**
  * struct dlistint_s - doubly linked list
