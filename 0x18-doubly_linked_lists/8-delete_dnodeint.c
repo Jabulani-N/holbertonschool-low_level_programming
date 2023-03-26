8ff9a60dae44316de9b15087703ccf8069cb5a08
Jabulani-N <jabu.94@gmail.com>
2023-03-25 19:25:58 -0700

    cleaned_if_protection

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -31,14 +31,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	} /*we're now on the node we want to delete*/
 
 	if (hold->next)
+	{
 		if (hold->prev)/*there is both node sbefore and after*/
 			(hold->next)->prev = (hold->prev);
 		else/*node after but not before*/
 			hold->next->prev = NULL;
+	}
 	else
+	{
 		if (hold->prev)/*node before but not after*/
 			hold->prev->next = NULL
-	/*neither previous nor next is a lone node node in default state*/
+	} /*if none, it is a lone node node in default state*/
 				/*it would need no change*/
 
 	/*now remove from history like Abdul Karim's letters*/
