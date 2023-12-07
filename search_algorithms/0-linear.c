d26db5935f76f3e1a09c5a8a541ff22717775083
Jabulani-N <jabu.94@gmail.com>
2023-12-06 16:05:24 -0800

    task0: added print statement ofr each index item

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,33 +1,48 @@
 #include "search_algos.h"
 
 /**
- *linear_search - returns index of array array containing value value
- * A string is an array of characters
- * @array: the array that'll be investigsted
- * @size: the length of array
- * @value: value to find. int.
- * Return: index containing value
- **/
+	*linear_search - returns index of array array containing value value
+	* A string is an array of characters
+	* @array: the array that'll be investigsted
+	* @size: the length of array
+	* @value: value to find. int.
+	* Return: index containing value
+	**/
 
-/*
-* Write a function that searches for a value in an array
-* of integers using the Linear search algorithm
-*
-* this means it just checks the list from front to back
-* and finishes when it either ends or when it finds a match
-*/
+	/*
+	* Write a function that searches for a value in an array
+	* of integers using the Linear search algorithm
+	*
+	* this means it just checks the list from front to back
+	* and finishes when it either ends or when it finds a match
+	*/
 
 int linear_search(int *array, size_t size, int value)
 {
-    /*
-    * *array is a pointer to the array
-    * we want to search through array
-    * return first index holding value
-    * if none hold value, or if value == NULL, return -1
-    *
-    * each check needs to print the position and value
-    *   see example in README.md
-    */
+	/*
+	* *array is a pointer to the array
+	* we want to search through array
+	* return first index holding value
+	* if none hold value, or if value == NULL, return -1
+	*
+	* each check needs to print the position and value
+	*   see example in README.md
+	*/
+	int i = 0;
+	size_t array_size = sizeof(array) / sizeof(array[0]);
 
-    return 0
+	while (array[i] != value && i < array_size)
+		/*while it is not the target value*/
+		/* and while it is still within range of array*/
+		/* be sure to do the print part that reports current index*/
+	{
+		printf("Value checked array[%i] = [%i]", i, array[i]);
+		i++;
+	}
+
+	if (array[i] == value)
+	{ /*if it actually found the right target, return the index*/
+		return (i);
+	}
+	return (-1);
 }
